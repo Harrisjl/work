1edfeadaa292a81f260d2c6560e1892559aba7f0
Harrisjl <jimmyl_harris@outlook.com>
2020-03-02 11:03:12 -0500

    update

--- homework/nodeweek5/week5server.js
+++ homework/nodeweek5/week5server.js
@@ -1,6 +1,6 @@
 //could not get either one to run
 
-var http = require('http')
+/*var http = require('http')
 var server = http.createServer((function (request, response) {
     response.writeHead(200,
         { "Content-Type" : "text/plain"});
@@ -12,12 +12,13 @@ var request = require("request");
 request("http://www.google.com", function (error, response, body) {
     console.log(body);
 });
+*/
 
 
-/*var http = require('http');
+var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World!');
+    res.write('Happy Coding World!');
     res.end();
 }).listen(8080);
 

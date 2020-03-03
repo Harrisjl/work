9d99c3bb67cd7d1dddde3835687543f42642f94a
harrisjl <jimmyl_harris@outlook.com>
2020-03-03 13:55:49 -0500

    week4

--- homework/week4.js
+++ homework/week4.js
@@ -3,7 +3,7 @@
 2. in that element give it a BEM class name
 3. add offset to a parent with a child
 4. position that child absolute and give it some offset
-make a grand child of step 3
-using z-index put the child of step 3 on top of the grand child
+5. make a grand child of step 3
+6. using z-index put the child of step 3 on top of the grand child
 */
 

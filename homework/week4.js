170fcbf4a7ce34ad79b3b701d11996cade08c7b5
harrisjl <jimmyl_harris@outlook.com>
2020-03-03 10:19:51 -0500

    StyleSheets update

---
+++ homework/week4.js
@@ -0,0 +1,9 @@
+/*
+1. give an element a block level class name
+2. in that element give it a BEM class name
+3. add offset to a parent with a child
+4. position that child absolute and give it some offset
+make a grand child of step 3
+using z-index put the child of step 3 on top of the grand child
+*/
+
